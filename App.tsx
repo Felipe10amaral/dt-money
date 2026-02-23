1f0cf21b567dd80cffc2e2734b656ae6323a04f8
Felipe Amaral <felipeamaraldev15@gmail.com>
2026-02-23 16:22:31 -0300

    configuracao de rotas

--- App.tsx
+++ App.tsx
@@ -1,8 +1,9 @@
 import './src/styles/global.css'
 import { Login } from './src/screen/Login';
+import NavigationRoutes from '@/routes';
 
 export default function App() {
   return (
-    <Login />
+   <NavigationRoutes />
   );
 }
--- src/screen/Login/index.tsx
+++ src/screen/Login/index.tsx
@@ -1,9 +1,17 @@
-import {View, Text} from 'react-native'
+import { PublicStackParamList } from '@/routes'
+import { useNavigation } from '@react-navigation/native'
+import { StackNavigationProp } from '@react-navigation/stack'
+import {View, Text, TouchableOpacity} from 'react-native'
 
 export const Login = () => {
+    const navigation = useNavigation<StackNavigationProp<PublicStackParamList>>()
+
     return (
         <View className='flex-1 items-center justify-center'>
             <Text>Login</Text>
+            <TouchableOpacity onPress={() => navigation.navigate('Register')}>
+                <Text>Registrar</Text>
+            </TouchableOpacity>
         </View>
     )
 }
