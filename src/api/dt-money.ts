93c2576faa5b38e0c294cd6b580d50f7039c5dee
Felipe Amaral <felipeamaraldev15@gmail.com>
2026-03-20 11:07:35 -0300

    configurando axios

---
+++ src/api/dt-money.ts
@@ -0,0 +1,11 @@
+import { Platform } from "react-native";
+import axios from "axios";
+
+const baseURL = Platform.select({
+    ios: "http://localhost:3001",
+    android: "http://10.0.2.2:3001"
+})
+
+export const dtMoneyAPI = axios.create({
+    baseURL
+})
