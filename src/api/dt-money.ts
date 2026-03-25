4ca536a8cf8dcfd3579c300a69eea6e904602ea0
Felipe Amaral <felipeamaraldev15@gmail.com>
2026-03-25 14:51:40 -0300

    classe de Errors

--- src/api/dt-money.ts
+++ src/api/dt-money.ts
@@ -1,5 +1,6 @@
 import { Platform } from "react-native";
 import axios from "axios";
+import { AppError } from "@/Errors/AppError";
 
 const baseURL = Platform.select({
     ios: "http://localhost:3001",
@@ -9,3 +10,15 @@ const baseURL = Platform.select({
 export const dtMoneyAPI = axios.create({
     baseURL
 })
+
+dtMoneyAPI.interceptors.response.use(
+    (config) => config,
+    (error) => {
+        if(error.response && error.response.data) {
+            return Promise.reject( new AppError(error.response.data.message))
+        }
+        else {
+            return Promise.reject( new AppError("Ocorreu um erro inesperado.")) 
+        }
+    }
+)
