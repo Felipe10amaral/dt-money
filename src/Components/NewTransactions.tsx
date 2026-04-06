2162751dd48e275efd8a81bada8b2c291a29d2c1
Felipe Amaral <felipeamaraldev15@gmail.com>
2026-04-06 15:48:20 -0300

    inputs

--- src/Components/NewTransactions.tsx
+++ src/Components/NewTransactions.tsx
@@ -2,9 +2,10 @@
 import {MaterialIcons} from '@expo/vector-icons'
 import { CreateTransactionsRequest } from "@/interface/https/createTransactionsRequest"
 import { useState } from "react"
-import { Text, TouchableOpacity, View } from "react-native"
+import { Text, TextInput, TouchableOpacity, View } from "react-native"
 import { colors } from '@/shared/colors'
 import { useBottomSheetContext } from '@/context/bottomSheet.context'
+import CurrencyInput from 'react-native-currency-input'
 
 export const NewTransactions = () => {
     const {closeBottomSheet} = useBottomSheetContext()
@@ -15,6 +16,12 @@ export const NewTransactions = () => {
         typeId: 0,
         value: 0
     })
+
+    const setTransactionsData = (key: keyof CreateTransactionsRequest, value: string | number) => {
+        setTransactions( (prevData) => ({...prevData, [key]: value}))
+    }
+
+   
     return (
         <View className='px-8 py-6'>
             <TouchableOpacity 
@@ -24,6 +31,26 @@ export const NewTransactions = () => {
                 <Text className='text-white text-xl font-bold'>Nova Transação</Text>
                 <MaterialIcons name="close" color={colors.gray["700"]} size={20} />
             </TouchableOpacity>
+            <View className='flex-1 mt-8 mb-8'>
+                <TextInput 
+                    className='text-white text-lg bg-background-primary my-2 rounded-lg-[6] pl-4 h-[50px]' 
+                    placeholder='Descrição'
+                    placeholderTextColor={colors.gray["700"]}
+                    value={transactions.description}
+                    onChangeText={(text) => setTransactionsData("description", text)}
+                />
+
+                <CurrencyInput 
+                    value={transactions.value}
+                    className='text-white text-lg bg-background-primary my-2 rounded-lg-[6] pl-4 h-[50px]'
+                    prefix='R$ '
+                    delimiter='.'
+                    separator=','
+                    precision={2}
+                    minValue={0}
+                    onChangeValue={(value) => setTransactionsData("value", value ?? 0)}
+                />
+            </View>
         </View>
     )
 }
